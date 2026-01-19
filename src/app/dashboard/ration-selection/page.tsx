c7f8c127ef0fef94ac7ea332c4559f6059fa5fea
NIRAN KUMAR <nirankumar2213@gmail.com>
2026-01-19 06:51:37 +0000

    make the celendar proper alignment

--- src/app/dashboard/ration-selection/page.tsx
+++ src/app/dashboard/ration-selection/page.tsx
@@ -143,7 +143,7 @@ export default function RationSelectionPage() {
                             </Button>
                           </FormControl>
                         </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0" align="center">
+                        <PopoverContent className="p-0" align="center">
                           <Calendar
                             mode="single"
                             selected={field.value}
