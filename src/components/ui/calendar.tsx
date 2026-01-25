450020fe5adb7640e4595bfcd2f0e9e255543c15
NIRAN KUMAR <nirankumar2213@gmail.com>
2026-01-25 12:42:22 +0000

    [{ 	"resource": "/home/user/studio/src/components/ui/calendar.tsx", 	"ow

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -51,8 +51,10 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      iconLeft={<ChevronLeft className="h-4 w-4" />}
-      iconRight={<ChevronRight className="h-4 w-4" />}
+      components={{
+        IconPrevious: () => <ChevronLeft className="h-4 w-4" />,
+        IconNext: () => <ChevronRight className="h-4 w-4" />,
+      }}
       {...props}
     />
   )
